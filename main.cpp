3a908815ec28f630a960e802f1caa5974ce97d68
Elias Daniel Bromerschenkel <82710406+EliasDaniel1@users.noreply.github.com>
2021-06-20 01:48:43 +0000

    1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "tComplexos.h"
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-}
+  cout << "Hello World!" << endl;
+
+  cout << "Calculadora de numeros complexos :)" << endl;
+
+  float a_real = 0;
+  float b_imaginario = 0;
+  float c_real = 0;
+  float d_imaginario = 0;
+
+  cout << "Digite o numero real de z1:";
+  cin >> a_real;
+  cout << "Digite o numero imaginario de z1:";
+  cin >> b_imaginario;
+  cout << "Digite o numero real de z2:";
+  cin >> c_real;
+  cout << "Digite o numero imaginario de z2:";
+  cin >> d_imaginario;
