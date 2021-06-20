9d8f7b35de0d6ccc7813b06dc88b21bd0c4d4ffb
Elias Daniel Bromerschenkel <82710406+EliasDaniel1@users.noreply.github.com>
2021-06-20 01:54:12 +0000

    4

--- main.cpp
+++ main.cpp
@@ -21,3 +21,27 @@ int main() {
   cin >> c_real;
   cout << "Digite o numero imaginario de z2:";
   cin >> d_imaginario;
+
+  tComplexos *calculadora = new tComplexos(a_real, b_imaginario, c_real, d_imaginario);
+
+  float soma1 = calculadora->soma_real();
+  float soma2 = calculadora->soma_imaginario();
+  float subtracao1 = calculadora->subtracao_real();
+  float subtracao2 = calculadora->subtracao_imaginario();
+  float multiplicacao1 = calculadora->multiplicacao_real();
+  float multiplicacao2 = calculadora->multiplicacao_imaginario();
+  int divisao1 = calculadora->divisao_real();
+  int divisao2 = calculadora->divisao_imaginario();
+  int modulo1 = calculadora->moduloz1();
+  int modulo2 = calculadora->moduloz2();
+  
+  
+  cout << "======================================================" << endl;
+  cout << "z1 + z2=" << " " << soma1 << "+" << soma2 << "i" << endl;
+  cout << "z1 - z2=" << " " <<subtracao1 << "+" << subtracao2 << "i" << endl;
+  cout << "z1 * z2=" << " " << multiplicacao1 << "+" << multiplicacao2 << "i" << endl;
+  cout << "z1 / z2=" << " " << divisao1 << "+" << divisao2 << "i" <<endl;
+  cout << "|Z1|=" << " " << modulo1 << endl;
+  cout << "|Z2|=" << " " << modulo2 << endl;
+}
+
